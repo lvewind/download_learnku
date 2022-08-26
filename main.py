380d40a72d9aa9f347dd459ce08ed6c38e239598
lvewind <lvewind@qq.com>
2022-08-27 00:14:26 +0800

    init

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from copy import deepcopy
 import os
-import time
-import json
 import pdfkit
 
 
